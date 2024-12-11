b279773a98e1dd5825d796716e9da60d9336802d
s2762697 <s2762697@ed.ac.uk>
2024-12-11 15:51:23 +0000

    Add files via upload

--- flaskforwebsite.py
+++ flaskforwebsite.py
@@ -1,92 +1,100 @@
 """ TIGIS Flask app for demonstration and learning
     To run on XRDP
-    geos-flask --app myflaskapp run --debug
-
-    See https://flask.palletsprojects.com/en/stable/quickstart/
-"""
-
+    geos-flask --app myflaskapp run --debug"""
+import json
+import sys
+import os
+import logging
+from pathlib import Path
+import cgitb
 from flask import Flask
 from flask import render_template
-from pathlib import Path
-import json
-import os
-import sys
-import logging
-from flask import url_for #can delete if not used
 import cx_Oracle
-import cgitb
+
 
 app = Flask(__name__)
 
 def get_password(passfile='tigis/Rwebmap/database_password'):
     """Reads a password from a file in stored in the home directory of the user.
     Args:
-        passfile (str, optional): A filename containing the password in the users home directory
+        passfile: A file containing the password in the users home directory
     Returns:
-        str: a password in plain text
+        passw: password in plain text
     Raises:
         FileNotFoundError: if the password file does not exist
-    """     
+    """
     home = Path.home()
     passfile = os.path.join(home, passfile)
     try:
         with open(passfile) as file:
-            pw = file.readline().strip()
-        return pw
+            passw = file.readline().strip()
+        return passw
     except FileNotFoundError as detail:
         logging.error(f'Password file not found: {detail}')
         sys.exit()
 
 @app.route('/')
 def index():
-    message = """Hello world. This is a flask app. 
+    """Prelim page - EDIT LATER"""
+    message = """Hello world. This is a flask app.
             Explore the other pages by appending the various @app.route arguments to this URL."""
     return message
 
-@app.route('/trees') 
+@app.route('/trees')
 def retreive():
-    """ Display Oracle table in an HTML table """
+    """ Display Oracle table in an HTML table
+    Args:
+        password: gets password and reads from passfile
+        connect: connect to specified Oracle database
+        execute: SQL query command
+    Returns:
+        table: data selected from the Oracle database
+    Raises:
+        ADD AN ERROR
+    """
     cgitb.enable()      #not sure if need or not
     password = get_password()
 
     connect = cx_Oracle.connect(dsn="geoslearn", user="s2762697",password=password)
-    with connect.cursor() as c:
-        c.execute("select * from s2750126.trees")   #sql query
-        data = c.fetchall()
-        #present data 
+    with connect.cursor() as cur:
+        cur.execute("select * from s2750126.trees")   #sql query
+        data = cur.fetchall()
+        #present data
         column_headings = tuple() #need to fix
     title = "Roseburn Tree data - from an Oracle Database" ##DONT HAVE COLUMN HEADINGS
     return render_template('table1.html', table=data, title=title)
+#except FileNotFoundError as detail:
+#    logging.error(f'Connection to Database is not possible: {detail}')
+#    sys.exit()
 
-    #c.execute("SELECT Latitude FROM s2750126.trees")
-    #for row in c:
-    #    print("<p>",row[0],"</p>")
-    #    latitude = c.fetchall()
-    #c.execute("SELECT Longitude FROM s2750126.trees")
-    #for row in c:
-    #    print("<p>",row[0],"</p>")
-    #    longitude = c.fetchall()
-    ##print(latitude)
-    ##print(longitude)
-    #return render_template('table1.html', table=data)
-    ##connect.close()
-    
 @app.route('/markers')
 def map():
+    """ Display tree markers on a map, using coordinates from the oracble database
+    Args:
+        password: gets password and reads from passfile
+        connect: connect to specified Oracle database
+        execute: SQL query command
+    Returns:
+        markers: markers using the coordinates given in the oracle database
+    Raises:
+        ADD AN ERROR
+    """
     password = get_password()
     connect = cx_Oracle.connect(dsn="geoslearn", user="s2762697",password=password)
-    with connect.cursor() as c:
-            c.execute("select * from s2750126.trees")     #sql query
-            data = c.fetchall()
-            for i in range(0,34):
-                print(data[i][2])       #remove later
-    with open('/home/s2762697/tigis/Rwebmap/static/boundary.json') as f:
-        geojson_data = json.load(f)   
+    with connect.cursor() as cur:
+        cur.execute("select * from s2750126.trees")     #sql query
+        data = cur.fetchall()
+        for i in range(0,34):
+            print(data[i][2])       #remove later
+    with open('/home/s2762697/tigis/Rwebmap/static/boundary.json') as file:
+        geojson_data = json.load(file)
     #geojson_data=json.dumps(geojson_data)
     return render_template('leafletmarkers.html',markers=data,geojson_data=json.dumps(geojson_data))
-
-
     #return render_template('leafletmarkers_copy2.html',markers=data)
+#except FileNotFoundError as detail:
+#    logging.error(f'Connection to Database is not possible: {detail}')
+#    sys.exit()
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=55403 , debug=True)
